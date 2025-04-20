72ca94666ef2a10520e9a96ffad21fe0d701810b
areebatahi <areebatahirarain@gmail.com>
2025-04-21 02:12:19 +0500

    fix error

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173'],
+		origin: ['http://localhost:5174', 'http://localhost:5173','https://ecomerce-website-vert.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
